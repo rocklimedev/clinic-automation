98155b1608fd910cf30b36602e4e97ff9225a71d
devrocklimers <devrocklimers@gmail.com>
2026-08-07 17:12:50 +0530

    added completed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-
+  /** */
   app.enableCors({
     origin: 'http://localhost:5174',
     credentials: true,
